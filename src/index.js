df44646a2f4dc325b6a3f50b7dd4bef5f7a8e4dd
Luigi Llanos <59068475+Luigi-Llanos@users.noreply.github.com>
2019-12-31 13:41:19 -0500

    new library
    create a new library with a character limit of 100

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-var numbers = [3, 56, 2, 48, 5];
+import emojipedia from "./emojipedia";
+
+console.log(emojipedia);
+
+const newLibrary = emojipedia.map(function(emoji) {
+  return emoji.meaning.substring(1, 100);
+});
+
+console.log(newLibrary);
+
+//var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 /*const newNumbers = numbers.map(function(num) {
@@ -33,8 +43,8 @@ console.log(newNumbers);
    */
 
 //FindIndex - find the index of the first item that matches.
-const newNumbers = numbers.findIndex(function(num) {
+/*const newNumbers = numbers.findIndex(function(num) {
   return num > 10;
 });
 
-console.log(newNumbers);
+console.log(newNumbers);*/
